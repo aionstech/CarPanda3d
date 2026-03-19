80bfe5d2865b2958d40b28eb2f526c7ecaa70c3d
Soud Ali <info.aions@gmail.com>
2026-03-18 20:40:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,3 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import Vec3
 from direct.task import Task
 
-#bx2egg /car/car.fbx -o /car/car.bam
-
-import panda3d
-print(panda3d.__file__)
